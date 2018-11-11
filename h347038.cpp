e5b51c6e434abd23cf444dd00b60984a6e4b4509
Bence Gobolos <bencegobolos@gmail.com>
2018-11-11 20:37:32 +0100

    Create initial .cpp file

---
+++ h347038.cpp
@@ -0,0 +1,75 @@
+// === MEGVALÓSÍTANDÓ ===
+
+/**
+ * Az osztálysablon neve my_tree.
+ * A sablon paraméterezése:
+ *   - T: a fapontokban tárolt adat típusa
+ * A sablonpéldány a tree_base osztályból származik (amely rendelkezik default
+ * konstruktorral). A beadott feladatban a tree_base osztályt nem kell (nem
+ * szabad) megvalósítani!
+ */
+//template<class T>
+//class my_tree : tree_base {
+template<class T>
+class my_tree {
+private:
+    struct node {
+        T data;
+        node* left;
+        node* right;
+    };
+
+    node* root;
+
+public:
+    /**
+     * Belsõ osztály, amely az inorder bejárást biztosító iterátort valósítja meg.
+     */
+    class iterator;
+    /** A sablonpéldány rendelkezik default konstruktorral. */
+    my_tree();
+    /**
+     * A sablonpéldány rendelkezik olyan konstruktorral, amely inicializálja a
+     * fapontban tárolt adatot.
+     */
+    my_tree(const T &data);
+    /**
+     * A sablonpéldány rendelkezik olyan konstruktorral, amely inicializálja a
+     * fapontban tárolt adatot, és a bal és jobb gyerekét.
+     */
+    my_tree(const T &data, my_tree<T> *left, my_tree<T> *right);
+    /** A destruktor felszabadítja a teljes részfát. */
+    ~my_tree();
+    /** Hozzáférés a fapontban tárolt adathoz. */
+    const T& data() const;
+    T& data();
+    /** A fapont szülõje. */
+    my_tree<T>* parent() const;
+    /** A fapont bal gyereke. */
+    my_tree<T>* leftChild() const;
+    /** A fapont jobb gyereke. */
+    my_tree<T>* rightChild() const;
+    /** Beállítja a fapont bal gyerekét. */
+    void setLeftChild(my_tree<T> *left);
+    /** Beállítja a fapont jobb gyerekét. */
+    void setRightChild(my_tree<T> *right);
+    /** Iterátor típus, amely inorder bejárást biztosít a fában. */
+    class iterator;
+    /** Iterátor a fa inorder bejárás szerinti elsõ elemére. */
+    iterator begin();
+    /** Iterátor a fa inorder bejárás szerinti utolsó eleme utáni elemre. */
+    iterator end();
+};
+
+// === MEGVALÓSÍTÁS VÉGE ===
+
+/**
+ * Példa a fa adatszerkezet felhasználására. A beadott feladatban nem szabad
+ * szerepelnie.
+ */
+int main() {
+    //my_tree<int> *tree = new my_tree<int>(8, new my_tree<int>(7), new my_tree<int>(9));
+    //cout << *tree->begin();
+
+    return 0;
+}
