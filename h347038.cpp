9565e1ef49462c5204bcd3414842e1d2100b9cb0
Bence Gobolos <bencegobolos@gmail.com>
2018-11-15 02:00:44 +0100

    Implement 3-param constructor

--- h347038.cpp
+++ h347038.cpp
@@ -44,7 +44,11 @@ public:
         this->left = nullptr;
         this->right = nullptr;
     }
-    my_tree(const T &data, my_tree<T> *left, my_tree<T> *right);
+    my_tree(const T &data, my_tree<T> *left, my_tree<T> *right) {
+        this->d = data;
+        this->left = left;
+        this->right = right;
+    }
     /** A destruktor felszabadítja a teljes részfát. */
     ~my_tree() {
         if (this->left != nullptr) {
@@ -106,5 +110,11 @@ int main() {
     tree2.setRightChild(&tree3);
     cout << "tree2 left child (tree): " << tree2.leftChild()->data() << endl;
     cout << "tree2 right child (tree3): " << tree2.rightChild()->data() << endl;
+
+    my_tree<int> t_left = my_tree<int>(0);
+    my_tree<int> t_right = my_tree<int>(2);
+    my_tree<int> t0 = my_tree<int>(1, &t_left, &t_right);
+    cout << t0.leftChild()->data() << " <- " << t0.data() << " -> " << t0.rightChild()->data() << endl;
+
     return 0;
 }
