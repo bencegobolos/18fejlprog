0603b58a46a3ae64006ce9875e7b3e8f5a94ee3b
Bence Gobolos <bencegobolos@gmail.com>
2018-11-17 02:31:15 +0100

    [BIRO] New upload (6)

--- beadott/h347038.cpp
+++ beadott/h347038.cpp
@@ -104,10 +104,14 @@ public:
     my_tree(const T &data, my_tree<T> *left, my_tree<T> *right) {
         this->d = data;
         this->p = nullptr;
-        this->left = left;
-        this->left->p = this;
-        this->right = right;
-        this->right->p = this;
+        if (left->data() < this->data()) {
+            this->left = left;
+            this->left->p = this;
+        }
+        if (right->data() > this->data()) {
+            this->right = right;
+            this->right->p = this;
+        }
     }
     /** A destruktor felszabadítja a teljes részfát. */
     ~my_tree() {
@@ -137,13 +141,17 @@ public:
     }
     /** Beállítja a fapont bal gyerekét. */
     void setLeftChild(my_tree<T> *left) {
-        this->left = left;
-        this->left->p = this;
+        if (left->data() < this->data()) {
+            this->left = left;
+            this->left->p = this;
+        }
     }
     /** Beállítja a fapont jobb gyerekét. */
     void setRightChild(my_tree<T> *right) {
-        this->right = right;
-        this->right->p = this;
+        if (right->data() > this->data()) {
+            this->right = right;
+            this->right->p = this;
+        }
     }
 };
 
